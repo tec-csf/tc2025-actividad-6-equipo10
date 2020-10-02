4569ebd1c25687c8ae543fd3affaa0d177d448ac
Sergio Hernandez Castillo <shcastillo@MacBook-Pro.lan>
2020-10-01 21:52:57 -0500

    Aun no sirve, pero se hizo progreso.

--- consola.c
+++ consola.c
@@ -3,7 +3,8 @@ Autores: Antonio Junco de Haas, Sergio Hernandez Castillo
 Matrículas: A, A01025210
 Descripción: Actividad 6 - Sockets y Señales
 
-127.0.0.1
+NOTA: Se trabajó junto con el equipo 9, el cual está compuesto por Daniel Roa
+      y Miguel Monterrubio
 */
 
 #include <stdio.h>
@@ -12,15 +13,40 @@ Descripción: Actividad 6 - Sockets y Señales
 #include <arpa/inet.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <signal.h>
+#include <sys/types.h>
 
 #define TCP_PORT 8000
+
+int semaforo_actual;
+
+void todosEnRojo(int senial){
+
+}
+
+void todosEnAmarillo(int senial){
+
+}
+
+void estadoSemaforoActual(int senial){
+    printf("\n");
+
+    for (int i = 0; i < 4; ++i){
+        if (i == senial){
+            printf("Semáforo %d está en verde.\n", i + 1);
+        }
+
+        else {
+            printf("Semáforo %d está en rojo.\n", i + 1);
+        }
+    }
+}
 
 int main(int argc, const char * argv){
     struct sockaddr_in direccion;
     char buffer[1000];
+    char inicio[] = "INICIO";
     int servidor;
-    int cliente;
-    int continuar = 1;
     ssize_t leidos;
     ssize_t escritos; 
     pid_t pid;
@@ -41,9 +67,58 @@ int main(int argc, const char * argv){
     bind(servidor, (struct sockaddr *) &direccion, sizeof(direccion));
 
     // Escuchar 
-    listen(servidor, 4); // Porque son 4 semaforos
+    listen(servidor, 4); // 4 porque son 4 semaforos
 
     escritos = sizeof(direccion);
+
+    int semaforos[4];
+    int bufferes_semaforos[4];
+    ssize_t pids[4];
+
+    for (int i = 0; i < 4; ++i){
+        semaforos[i] = accept(servidor, (struct sockaddr *) &direccion, &escritos);
+        printf("Aceptando conexiones en %s:%d\n", inet_ntoa(direccion.sin_addr), ntohs(direccion.sin_port));
+
+        pid = fork();
+
+        if (pid == 0){
+            semaforo_actual = semaforos[i];
+            signal(SIGTSTP, todosEnRojo);
+            //signal(SIGINT, todosEnAmarillo);
+
+            close(servidor);
+
+            if (semaforo_actual >= 0){
+                while(leidos = read(semaforos[i], &buffer, sizeof(buffer))){
+                    estadoSemaforoActual(i);
+                }
+            }
+
+            close(semaforo_actual);
+        }
+
+        else {
+            pids[i] = read(semaforos[i], &bufferes_semaforos[i], sizeof(bufferes_semaforos[i]));
+        }
+    }
+
+    if (pid > 0){
+        for (int i = 0; i < 4; ++i){
+            if (i == 3){
+                write(semaforos[i], &bufferes_semaforos[0], pids[0]);
+            }
+
+            else {
+                write(semaforos[i], &bufferes_semaforos[i + 1], pids[i + 1]);
+            }
+        }
+
+        write(semaforos[0], &inicio, sizeof(inicio));
+
+        while (wait(NULL) != -1);
+
+        close(servidor);
+    }
 
     return 0;
 }
